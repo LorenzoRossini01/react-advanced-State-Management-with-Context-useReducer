2a2c8b875b28b63b62c7451348579b712dd518fc
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-07-04 17:47:33 +0200

    feat: use useContext Hook to pass data between components instead of props, use useReducer to update state instead of useState

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
-import CartContext from "../store/shopping-cart-context";
+import { CartContext } from "../store/shopping-cart-context";
 
 export default function Product({ id, image, title, price, description }) {
-  const cartCtx = useContext(CartContext);
+  const { addItemToCart } = useContext(CartContext);
 
   return (
     <article className="product">
@@ -14,7 +14,7 @@ export default function Product({ id, image, title, price, description }) {
           <p>{description}</p>
         </div>
         <p className="product-actions">
-          <button onClick={() => cartCtx.addItemToCart(id)}>Add to Cart</button>
+          <button onClick={() => addItemToCart(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
